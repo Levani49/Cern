c9f006e60120b042933556aa70d6fc41a033f8be
ikverenc <kverenchkhiladzeirakli.personal@gmail.com>
2023-04-02 23:45:48 +0400

    reconfigured eslint, updated images

--- src/components/logo/Logo.component.tsx
+++ src/components/logo/Logo.component.tsx
@@ -1,10 +1,8 @@
-///tracer-logo.png
-
 export default function Logo(): JSX.Element {
   return (
     <div className="absolute bottom-[1px] left-1 select-none">
       <div className="uppercase font-medium text-light cursor-pointer h-16 flex justify-center">
-        <img src="/tracer-logo.png" alt="logo" height={65} width={100} />
+        <img src="/images/tracer-logo.png" alt="logo" height={65} width={100} />
       </div>
     </div>
   );
