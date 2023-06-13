dbdfe9d88a7d99f3b5b166b96f7e68b073a1adac
nzurashv <nino.zurashvili@cern.ch>
2023-06-13 11:33:20 +0400

    init

--- src/components/tree/childNode/ChildNode.component.tsx
+++ src/components/tree/childNode/ChildNode.component.tsx
@@ -2,7 +2,7 @@ import { useAppDispatch } from "@store/hooks";
 
 import { updateChildNodeState } from "@features/tree/treeSlice";
 
-import { GeometryState } from "../../../constants/geometryTree";
+import { GeometryState } from "@constants/geometryTree";
 
 export interface ChildNodeProps {
   uid: string;
@@ -46,13 +46,14 @@ export default function ChildNode({
   return (
     <li
       className={`flex items-center border-l-[1px] border-white ${
-        nodeEnd && "last-event-line border-l border-solid border-transparent"
+        nodeEnd &&
+        "last-child-event-line border-l border-solid border-transparent"
       } ${innerState}`}
     >
       <span
         role="presentation"
         onClick={onClickHandler}
-        className="relative left-0 ml-[-0.5px] cursor-pointer overflow-ellipsis whitespace-nowrap border-white text-left align-middle text-xs uppercase transition before:relative before:mr-[2px] before:inline-block before:w-[15px] before:border before:border-t-[0.5px] before:align-middle before:text-white"
+        className="relative left-0 ml-[-0.5px] cursor-pointer overflow-ellipsis whitespace-nowrap border-white text-left align-middle text-xs uppercase transition before:relative before:mr-[2px] before:inline-block before:w-[15px]  before:border-0 before:border-t-[0.5px] before:align-middle before:text-white"
       >
         {name}
       </span>
--- src/components/tree/parentNode/ParentNode.component.tsx
+++ src/components/tree/parentNode/ParentNode.component.tsx
@@ -65,7 +65,7 @@ export default function ParentNode({
       ? "text-yellow-500"
       : "text-white";
 
-  const styles = `border-l-[1px] border-transparent text-left transition before:relative before:inline-block before:w-[15px] before:left-[-1px] before:align-middle before:border before:border-t-[0.5px] before:border-0 before:text-white before:align-middle ${
+  const styles = `border-l-[1px] border-transparent text-left transition before:relative before:inline-block before:w-[15px]  before:align-middle before:border before:border-t-[0.5px] before:border-0 before:text-white before:align-middle ${
     nodeEnd ? "last-event-line" : ""
   } ${root && "before:opacity-0"}`;
 
