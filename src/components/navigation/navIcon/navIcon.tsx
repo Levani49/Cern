99dec3f3b43490c6498d8d81184b48f83eee5893
ikverenc <kverenchkhiladzeirakli.personal@gmail.com>
2023-06-20 11:33:03 +0400

    besos notes

--- src/components/navigation/navIcon/navIcon.tsx
+++ src/components/navigation/navIcon/navIcon.tsx
@@ -20,9 +20,7 @@ export default function NavIcon({
       {...other}
     >
       <Icon
-        className={`icon ${
-          active && "text-blue dark:text-green"
-        } transition-all ${iconColor}`}
+        className={`icon ${active && "text-blue dark:text-green"} transition-all ${iconColor}`}
       />
     </button>
   );
