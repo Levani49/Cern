13cac9ad027071cb631a33020f4beaf15e15073a
nzurashv <nino.zurashvili@cern.ch>
2023-06-22 11:00:12 +0400

    updated nav design

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -14,7 +14,7 @@ import WireframeMenu from "./wireframe/WireframeMenu.component";
 
 export default function Navigation(): JSX.Element {
   return (
-    <div className="fixed top-0 z-50 flex w-full flex-auto select-none select-none items-center justify-center">
+    <div className="fixed top-0 z-50 flex w-full flex-auto select-none items-center justify-center">
       <nav className="flex w-auto items-center gap-2 overflow-x-auto rounded-sm bg-customGray py-2 pl-4 pr-4">
         <NavTitle />
         <GeometryMenu />
