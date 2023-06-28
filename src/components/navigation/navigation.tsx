63d0890824dd230534963ca9ce2ae542f32ee2bc
nzurashv <nino.zurashvili@cern.ch>
2023-06-28 22:33:35 +0400

    init

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -2,24 +2,23 @@ import { useAppSelector } from "@store/hooks";
 
 import { selectMenuBar } from "@features/global/globalsSlice";
 
+import AboutMenu from "@components/navigation/about/AboutMenu.component";
+import CameraViewMenu from "@components/navigation/cameraView/CameraViewMenu.component";
+import DroneMenu from "@components/navigation/drone/DroneMenu.component";
+import EventsMenu from "@components/navigation/events/EventsMenu.component";
+import FullScreenMenu from "@components/navigation/fullscreen/FullScreenMenu.component";
+import GeometryMenu from "@components/navigation/geometry/GeometryMenu.component";
+import GeometryCutsMenu from "@components/navigation/geometryCuts/GeometryCutsMenu.component";
 import Link from "@components/navigation/link/link.component";
 import NavigationBar from "@components/navigation/navigationBar/navigationToggler";
+import NavTitle from "@components/navigation/navTitle/navTitle.component";
+import OpacirtyMenu from "@components/navigation/opacity/OpacityMenu.component";
 import Settings from "@components/navigation/settings/settings";
 import ShowMenuBar from "@components/navigation/showMenuBar/showMenuBar";
+import ThemeToggler from "@components/navigation/theme/ThemeToggler.component";
+import WireframeMenu from "@components/navigation/wireframe/WireframeMenu.component";
 
 import { isMobile } from "@utils/isMobile.utils";
-
-import AboutMenu from "./about/AboutMenu.component";
-import CameraViewMenu from "./cameraView/CameraViewMenu.component";
-import DroneMenu from "./drone/DroneMenu.component";
-import EventsMenu from "./events/EventsMenu.component";
-import FullScreenMenu from "./fullscreen/FullScreenMenu.component";
-import GeometryMenu from "./geometry/GeometryMenu.component";
-import GeometryCutsMenu from "./geometryCuts/GeometryCutsMenu.component";
-import NavTitle from "./navTitle/navTitle.component";
-import OpacirtyMenu from "./opacity/OpacityMenu.component";
-import ThemeToggler from "./theme/ThemeToggler.component";
-import WireframeMenu from "./wireframe/WireframeMenu.component";
 
 export default function Navigation(): JSX.Element {
   const showMenuBar = useAppSelector(selectMenuBar);
