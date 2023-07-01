5cdff5e43f67056c89690c787f13ff49a3771803
nzurashv <nino.zurashvili@cern.ch>
2023-07-01 23:16:32 +0400

    implemented etags

--- server.js
+++ server.js
@@ -9,15 +9,14 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const YEAR = 31536000;
 
 app.use(compression());
-app.use((_, res, next) => {
-  res.setHeader("Cache-Control", `max-age=${YEAR}`);
-  next();
-});
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(
+  express.static(path.join(__dirname, "dist"), {
+    etag: true
+  })
+);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
