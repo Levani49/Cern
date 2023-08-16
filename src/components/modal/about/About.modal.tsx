35d672855525f19d09a2436df5102e1f1566ea5a
nzurashv <nino.zurashvili@cern.ch>
2023-08-16 19:08:22 +0400

    init

--- src/components/modal/about/About.modal.tsx
+++ src/components/modal/about/About.modal.tsx
@@ -62,8 +62,8 @@ export default function AboutModal(): JSX.Element {
         </div>
 
         <p className="mt-6 flex justify-center text-xs">
-          Application is built on three.js version{" "}
-          <span className="ml-1 text-accent1">0.154.0</span>
+          The application is built on three.js version{" "}
+          <span className="ml-1 text-accent2 dark:text-accent1">0.154.0</span>
         </p>
 
         <div className="mt-6 flex flex-col items-center gap-2 text-xs">
