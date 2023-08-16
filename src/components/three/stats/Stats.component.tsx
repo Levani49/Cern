29326d93d07cc09659e3cc47ebe6527c47336a7e
nzurashv <nino.zurashvili@cern.ch>
2023-08-16 12:01:05 +0400

    fixed stats

--- src/components/three/stats/Stats.component.tsx
+++ src/components/three/stats/Stats.component.tsx
@@ -10,7 +10,7 @@ export default function Stats(): JSX.Element {
   return (
     <>
       {show && (
-        <div className="absolute bottom-auto left-2 top-2 z-[9999] flex flex transform select-none flex-col gap-2 text-xs font-normal text-white sm:bottom-[1px] sm:left-1/2 sm:left-auto sm:top-auto sm:-translate-x-1/2 sm:-translate-y-1/2 sm:flex-row">
+        <div className="absolute bottom-auto left-2 top-2 z-[9999] flex transform select-none flex-col gap-2 text-xs font-normal text-white sm:bottom-[1px] sm:left-1/2  sm:top-auto sm:-translate-x-1/2 sm:-translate-y-1/2 sm:flex-row">
           <span>TRIANGLES: {triangles}</span>
           {memory ? (
             <span className="hidden sm:block">MEMORY: {memory} MB</span>
