aa394fb95ec7a9dff3e38fc99a052e72aed634fa
nzurashv <nino.zurashvili@cern.ch>
2023-09-09 21:58:10 +0400

    init

--- src/components/Logo.component.tsx
+++ src/components/Logo.component.tsx
@@ -3,7 +3,7 @@ export default function Logo() {
     <div className="absolute left-1/2 top-2 -translate-x-1/2 transform select-none sm:bottom-[1px] sm:left-4  sm:top-auto sm:translate-x-[0]">
       <div className=" flex h-16 cursor-pointer justify-center font-medium uppercase text-textColor">
         <img
-          src="/assets/images/tracer-logo.webp"
+          src="/images/tracer-logo.webp"
           alt="tracer logo"
           height={65}
           width={100}
