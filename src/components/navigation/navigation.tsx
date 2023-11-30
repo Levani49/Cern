2952c48ce1a16ca37e5e7e5d538442a4937093cb
Levani49 <levani.pachuashvili.1@btu.edu.ge>
2023-11-30 20:23:09 +0400

    remove events menu

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -27,7 +27,6 @@ export default function Navigation() {
             <NavTitle />
             <NavigationBar />
             <GeometryMenu />
-            <EventsMenu />
             <DroneMenu />
             <CameraViewMenu />
             <GeometryCutsMenu />
